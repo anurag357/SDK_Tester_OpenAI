0431c996468b84ff0eb07292e45874549cdf8afd
anurag357 <89999226+anurag357@users.noreply.github.com>
2025-08-26 21:36:15 +0530

    fixed puppeteer remove

--- app/api/automate/route.js
+++ app/api/automate/route.js
@@ -44,7 +44,7 @@ export async function GET() {
     if (process.env.VERCEL === "1") {
       // Vercel serverless
       const { chromium } = require("playwright-aws-lambda");
-        browser = await chromium.puppeteer.launch({
+        browser = await chromium.launch({
         args: chromium.args,
         executablePath: await chromium.executablePath(),
         headless: true,
